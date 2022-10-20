a838e82347cee45a1d36047b8de6a22a8de9c375
alexhond <a3836161@gmail.com>
2022-10-20 22:55:14 +0300

    рефакторинг

--- jenkins/otus-ui-autotests.groovy
+++ jenkins/otus-ui-autotests.groovy
@@ -4,7 +4,7 @@ timeout(300) {
             checkout scm
         }
         stage('Running tests') {
-            sh "mvn test -Dbase.url=${BASE_URL} -Dbrowser=${BROWSER_NAME} -DincludeTags=${TAGS} -Dwebdriver.remote.url=http://localhost/wd/hub"
+            sh "mvn test -Dbase.url=${BASE_URL} -Dbrowser=${BROWSER_NAME} -DincludeTags=${TAGS}"
         }
         stage('Publisher allure') {
             allure([
